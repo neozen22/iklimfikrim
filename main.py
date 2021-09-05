472313647951aa209462dd63bb4993ea58b8e02f
Can <av.cancelik@gmail.com>
2021-09-05 20:44:42 +0300

    -added article reading system -implemented a tags for articles on main pages -fixed bugs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request, jsonify
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 import os
@@ -11,15 +11,14 @@ class RegisterForm(Form):
     password = PasswordField("Şifre", validators=[validators.Length(1, 1, "Şifrede boşluk kullanmayınız"), validators.EqualTo("confirm", "Şifre eşleşmiyor")])
     confirm = PasswordField("Şifre Doğrula")
 
-with open("data/articles.json") as articles_file:
-    articles = json.load(articles_file)
-
-for i in articles:
-    print(i)
-
-
-
 app = Flask(__name__)
+
+
+def fetch_articles():
+    with open("static/data/articles.json", encoding="utf-8") as articles_file:
+        return json.load(articles_file)
+
+# articles = fetch_articles()
 
 #SQL CONFIGURATION
 # app.config["MYSQL_HOST"] = "localhost"
@@ -32,12 +31,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", articles=[i for i in range(10)])
+    articles = fetch_articles()
+    
+    return render_template("index.html", articles=articles)
 
 
 
 @app.route("/dashboard")
 def dashboard():
+    articles = fetch_articles()
     return render_template("dashboard.html", articles = articles)
 
 @app.route("/register", methods= ["GET", "POST"])
@@ -47,6 +49,14 @@ def register():
         pass
     else:
         return render_template("register.html", form= form)
+
+@app.route("/article/<string:id>")
+def article(id):
+    with open(f"static/data/article_assets/{id}/article.html","r", encoding="utf-8") as sj:
+        article_content = sj.read()
+    return render_template("articleholder.html",article_content=article_content)
+
+
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=True)
